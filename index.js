82b6f9a760b3789b735f91834e75b5e43f997c43
wahyu <128810262+wahyurifia@users.noreply.github.com>
2023-07-16 21:41:59 +0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const login = document.querySelector('#login');
+const close = document.querySelector('#close');
+const wrapper = document.querySelector('.wrapper');
+const body = document.body;
+const center = document.querySelector('.center')
+
+
+login.addEventListener('click', function(){
+    wrapper.classList.add("active")
+    body.classList.add('active')
+    center.classList.add('active')
+});
+
+close.addEventListener('click', function(){
+    wrapper.classList.remove("active")
+    body.classList.remove('active')
+    center.classList.remove('active')
+});
 
