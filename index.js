bcb5f56395d531b277e53e633cdc8ddcfe024afa
wahyu <128810262+wahyurifia@users.noreply.github.com>
2023-07-21 21:49:09 +0700

    memasukkan form login ke console Log

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const close = document.querySelector('#close');
 const wrapper = document.querySelector('.wrapper');
 const body = document.body;
 const center = document.querySelector('.center')
+const modal = document.getElementById('modal');
 
 
 login.addEventListener('click', function(){
@@ -16,4 +17,38 @@ close.addEventListener('click', function(){
     body.classList.remove('active')
     center.classList.remove('active')
 });
+
+
+modal.addEventListener('click', function(){
+    wrapper.classList.remove("active")
+    body.classList.remove('active')
+    center.classList.remove('active')
+    document.getElementById('modal').style.transform = 'translateY(-260%)'
+
+});
+
+let loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+  
+    if (username.value == "" || password.value == "") {
+    } else {
+      // perform operation with form input
+      console.log(
+        `Username = ${username.value}
+Password = ${password.value}`
+      );
+  
+      username.value = "";
+      password.value = "";
+
+      document.getElementById('wrapper').classList.remove("active")
+      document.getElementById('modal').style.transform = 'translateY(0%)'       
+    
+    }
+  });
 
